3d748bf64136a85d91a4ce0ef4b0d7319488aad6
Todd Gureckis <todd.gureckis@nyu.edu>
2011-12-20 23:27:52 +0000

    fixed up initial directory structure for Flask.

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, Response
+from string import split
+import os
+import time
+import datetime
+import os.path
+import sys
+from sqlalchemy import *
+from functools import wraps
+
+
+app = Flask(__name__)
+
+@app.route('/')
+#@requires_auth
+def indexroute():
+    return render_template('index.html')
+
+
+@app.route('/<pagename>.html')
+#@requires_auth
+def regularpage(pagename=None):
+    if pagename==None:
+        print "error"
+    else:
+        return render_template(pagename+'.html')
+
+
+###########################################################
+# let's start
+###########################################################
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print "Useage: python webapp.py [initdb/server]"
+    elif len(sys.argv)>1:
+        if sys.argv[1]=='initdb':
+            print "initializing database TBD"
+            pass
+        elif sys.argv[1]=='server':
+            print "starting webserver"
+            # by default just launch webserver
+            app.run(debug=False, port=5001)
+            
