714f484c89d71b145fe787f71d2ebd744f4e77bf
John McDonnell <john.mcdonnell@nyu.edu>
2012-01-21 06:54:23 +0000

    Added quals but launching tonight without.

--- mturk/createHIT.py
+++ mturk/createHIT.py
@@ -2,7 +2,7 @@ import datetime
 
 from boto.mturk.connection import MTurkConnection
 from boto.mturk.question import ExternalQuestion
-from boto.mturk.qualification import LocaleRequirement, PercentAssignmentsApprovedRequirement
+from boto.mturk.qualification import LocaleRequirement, PercentAssignmentsApprovedRequirement, Qualifications
 
 HOST = 'mechanicalturk.sandbox.amazonaws.com'
 
@@ -23,10 +23,9 @@ frameheight = 600
 mturkQuestion = ExternalQuestion( experimentPortalURL, 600 )
 
 # Qualification:
-#qualifications = [
-#    PercentAssignmentsApprovedRequirement(),
-#    LocaleRequirement
-#]
+quals = Qualifications()
+quals.add( PercentAssignmentsApprovedRequirement('GreaterThanOrEqualTo', 95) )
+quals.add( LocaleRequirement("Country", "US") )
 
 # Specify all the HIT parameters
 paramdict = dict(
@@ -34,15 +33,15 @@ paramdict = dict(
     question = mturkQuestion,
     lifetime = datetime.timedelta(1),  # How long the HIT will be available
     max_assignments = 1, # Total times it will be assigned, not max per turker
-    title = "Paid volunteers needed for an online experiment in Psychology",
-    description = "Learn to categorize a set of cards over a series of training trials.",
-    keywords = "New York University, psychology experiment, category learning",
+    title = "The Group Discovery Game (Psychology Experiment)",
+    description = "Learn to group a set of objects",
+    keywords = "psychology, learning, memory, cognition, experiment",
     reward = 1,
     duration = datetime.timedelta(1),
     approval_delay = None,
     annotation = None,  # Do we need this? Not clear on what it is.
     questions = None,
-    qualifications = None
+    qualifications = quals
 )
 
 myhit = mtc.create_hit(**paramdict )[0]
